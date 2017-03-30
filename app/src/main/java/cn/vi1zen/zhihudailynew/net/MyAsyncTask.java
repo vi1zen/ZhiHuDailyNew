6b021ea4908339471905cb455c67b56f02a12905
Vinnie <wenzhen.mail@foxmail.com>
2017-03-31 00:55:35 +0800

    增加首页轮播图

---
+++ app/src/main/java/cn/vi1zen/zhihudailynew/net/MyAsyncTask.java
@@ -0,0 +1,53 @@
+package cn.vi1zen.zhihudailynew.net;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by Destiny on 2017/3/30.
+ */
+
+public class MyAsyncTask extends AsyncTask<String,Void,String>{
+    @Override
+    protected String doInBackground(String... strings) {
+        OkHttpAsync.get(strings[0], new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i("LOADMORE","MyAsyncTask Fail...");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String resultStr = response.body().string();
+                try {
+                    JSONObject jsonObject = new JSONObject(resultStr);
+                    String date = jsonObject.getString("date");
+                    JSONArray stories = jsonObject.getJSONArray("stories");
+                    for(int i=0;i<stories.length();i++){
+                        JSONObject obj = (JSONObject) stories.get(i);
+                    }
+                    JSONArray top_stories = jsonObject.getJSONArray("top_stories");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
